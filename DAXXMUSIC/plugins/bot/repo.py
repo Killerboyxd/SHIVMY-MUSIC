e49d5c6cc7c91f5a4799d831125c06c790e18fd6
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-12-10 16:55:41 +0530

    Update repo.py

--- DAXXMUSIC/plugins/bot/repo.py
+++ DAXXMUSIC/plugins/bot/repo.py
@@ -29,11 +29,11 @@ async def start(_, msg):
           InlineKeyboardButton("Δᴅᴅ ᴍє", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("нєℓρ", url="https://t.me/ITZ_4_U/2"),
-          InlineKeyboardButton("σωиєя", url="https://t.me/ROCKY_ISS_BACK"),
+          InlineKeyboardButton("нєℓρ", url="https://t.me/RONNY_KI_DUNIYA"),
+          InlineKeyboardButton("σωиєя", url="https://t.me/ll_SABKA_BHAI_KILLER_ll/"),
           ],
                [
-                InlineKeyboardButton("Δʙᴏᴜᴛ ʀᴏᴄᴋყ", url="https://t.me/II_ROCKY_II"),
+                InlineKeyboardButton("Δʙᴏᴜᴛ sʜɪᴠ", url="https://t.me/@ll_DJ_BOL_ll),
 
 ],
 [
@@ -41,7 +41,7 @@ async def start(_, msg):
               InlineKeyboardButton("︎「ϰ∂ ⚚ иєƚωσɾƙ」", url=f"https://t.me/XD_N3TWORK"),
               ],
               [
-              InlineKeyboardButton("⏤͟͟͞͞ 𓆩𝐃ᴇᴄᴇɴᴛ ། ། 𝐂ʜᴀᴛɪɴɢ𓆪『 𝐂ʟᴜʙ 2", url=f"https://t.me/DECENT_CHATING"),
+              InlineKeyboardButton("⏤͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͟͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞CENT_CHATING"),
 InlineKeyboardButton("ᴅᴘ ᴡᴏʀʟᴅ", url=f"https://t.me/OFFICAL_DP_ZONE"),
 ],
 [
@@ -49,26 +49,26 @@ InlineKeyboardButton("sᴛʀɪɴɢʙᴏᴛ", url=f"https://t.me/XD_STRING_BOT"),
 InlineKeyboardButton("ғᴇᴅᴇʀᴀᴛɪᴏɴ", url=f"https://t.me/officeal_warrior_fedration"),
 ],
 [
-              InlineKeyboardButton("ᴍᴜsɪᴄ ʙᴏᴛ ʀᴇᴘᴏ", url=f"https://t.me/N91Ab/6"),
-              InlineKeyboardButton("sƚΔƚυs", url=f"https://t.me/About_Dangerous"),
+              InlineKeyboardButton("ᴍᴜsɪᴄ ʙᴏᴛ ʀᴇᴘᴏ", url=f"https://t.me/ll_SABKA_BHAI_KILLER_ll"),
+              InlineKeyboardButton("sƚΔƚυs", url=f"https://t.me/do_dil_ek_jaan143"),
               ],
               [
               InlineKeyboardButton("𝗞ɪᴅѕ 𝗢ғ 𝗧ᴇʟᴇɢʀᴀᴍ︎", url=f"https://t.me/ho_gya_viral"),
-InlineKeyboardButton("ɪɴsᴛᴀ", url=f"https://instagram.com/mr_._rocky_._01?igshid=MzNlNGNkZWQ4Mg=="),
+InlineKeyboardButton("ɪɴsᴛᴀ", url=f"https://instagram.com/killer_boy_op_hacker?igshid=OGQ5ZDc2ODk2ZA=="),
 ],
 [
 InlineKeyboardButton("ᴡʜᴀᴛsᴀᴘᴘ", url=f"https://wa.me/qr/474QNG5BZYHRM1"),
 InlineKeyboardButton("ᴜᴘᴄᴏᴍɪɴɢ", url=f"https://t.me/admin"),
 ],
 [
-InlineKeyboardButton("𝐏𝐀𝐍𝐃𝐀 | 𝐃𝐏𝐙 | 𝐄𝐃𝐈𝐓𝐙 ✨", url=f"https://t.me/PANDA_DPZ_EDITZ"),
+InlineKeyboardButton("𝐒𝐇𝐈𝐕 | 𝐃𝐏𝐙 | 𝐄𝐃𝐈𝐓𝐙 ✨", url=f"https://t.me/PANDA_DPZ_EDITZ"),
 
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://graph.org/file/360676a2c160029e03e46.jpg",
+        photo="https://telegra.ph/file/230a240669f5a1fa1a762.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
